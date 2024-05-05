1487235418c523f8f8199c01087886dc21a956c9
Sayyed Aamer Ali <sayyed.aamer.ali@gmail.com>
2024-05-05 10:50:01 +0530

    Initial Commit

---
+++ Youtube MVVM ProductUITests/Youtube_MVVM_ProductUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Youtube_MVVM_ProductUITestsLaunchTests.swift
+//  Youtube MVVM ProductUITests
+//
+//  Created by aamer sayyed on 05/05/24.
+//
+
+import XCTest
+
+final class Youtube_MVVM_ProductUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
